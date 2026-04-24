49d7dc4cf7fec4b0af4f1c80341a6fe53a8d8d07
xiaolan <36332075+yeqinwen@users.noreply.github.com>
2026-04-24 22:40:27 +0800

    Add files via upload

---
+++ demo_1_vis_mesh.py
@@ -0,0 +1,39 @@
+import numpy as np
+import polyscope as ps
+import pymeshlab
+
+ms = pymeshlab.MeshSet()
+
+ms.load_new_mesh('star300.obj')
+m = ms.current_mesh()
+star_v, star_f = m.vertex_matrix(), m.face_matrix()
+
+ms.load_new_mesh('scan.obj')
+m = ms.current_mesh()
+scan_v, scan_f = m.vertex_matrix(), m.face_matrix()
+
+ps.init()
+ps.register_surface_mesh("star", star_v, star_f,
+                         color=np.array([0, 91, 255]) / 255,
+                         edge_width=0.001,
+                         edge_color=[1, 1, 1],
+                         smooth_shade=True,
+                         # material="flat"
+                         )
+ps.register_surface_mesh("scan", scan_v, scan_f,
+                         color=np.array([255, 164, 0]) / 255,
+                         edge_width=0.001,
+                         edge_color=[1, 1, 1],
+                         smooth_shade=True,
+                         # material="flat"
+                         )
+
+# ps.set_view_projection_mode("orthographic")  # orthographic  perspective
+# ps.set_navigation_style("planar")  # ['turntable','free','planar','none','first_person']
+ps.set_ground_plane_mode("shadow_only")  # ['none','tile','tile_reflection','shadow_only']
+ps.set_shadow_blur_iters(3)
+ps.set_shadow_darkness(0.5)
+ps.set_up_dir("y_up")
+# ps.set_front_dir('x_front')
+ps.set_SSAA_factor(4)
+ps.show()
---
+++ demo_2_vis_error.py
@@ -0,0 +1,38 @@
+import pymeshlab
+import numpy as np
+import polyscope as ps
+
+ms = pymeshlab.MeshSet()
+ms.load_new_mesh('star300.obj')
+ms.load_new_mesh('scan.obj')
+
+# hausdorff_distance
+ms.get_hausdorff_distance(targetmesh=0, sampledmesh=1, samplevert=True)
+dis = ms.mesh(1).vertex_scalar_array()
+
+vmin = 0
+vmax = np.percentile(dis, 95)  # p95 or p99
+
+m = ms.current_mesh()
+scan_v, scan_f = m.vertex_matrix(), m.face_matrix()
+
+ps.init()
+body = ps.register_surface_mesh("scan", scan_v, scan_f, edge_width=0.001, smooth_shade=True)
+body.add_scalar_quantity("distance", dis,
+                         defined_on="vertices",
+                         enabled=True,
+                         # vminmax=[vmin, vmax],
+                         vminmax=[0, 0.008],
+                         cmap="jet",  # my_colormap
+                         onscreen_colorbar_enabled=True  # show the colorbar
+                         )
+
+# ps.set_view_projection_mode("orthographic")  # orthographic  perspective
+# ps.set_navigation_style("planar")  # ['turntable','free','planar','none','first_person']
+ps.set_ground_plane_mode("shadow_only")  # ['none','tile','tile_reflection','shadow_only']
+ps.set_shadow_blur_iters(3)
+ps.set_shadow_darkness(0.5)
+ps.set_up_dir("y_up")
+# ps.set_front_dir('x_front')
+ps.set_SSAA_factor(4)
+ps.show()
